15b106f2cdfe7892950f32e70f997d13b8f6e6c0
nathnaelg <ngirma700@gmail.com>
2023-11-30 22:18:09 +0300

    db connected

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const dotenv =require('dotenv');
 const port = 3000
+const mongoose = require("mongoose");
 
 
 dotenv.config()
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connected")).catch((err) => console.log(err))
 
 
 app.listen(process.env.PORT||port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
